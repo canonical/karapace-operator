d86f62dd6e80e6c942ddf110cc7de33963c2c3ee
Raul Zamora <raul.zamora@canonical.com>
2024-03-06 14:51:48 +0100

    initial commit

---
+++ tests/unit/test_charm.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import unittest
+
+import ops
+import ops.testing
+from charm import KarapaceCharm
+
+
+class TestCharm(unittest.TestCase):
+    def setUp(self):
+        self.harness = ops.testing.Harness(KarapaceCharm)
+        self.addCleanup(self.harness.cleanup)
+
+    def test_start(self):
+        # Simulate the charm starting
+        self.harness.begin_with_initial_hooks()
+
+        # Ensure we set an ActiveStatus with no message
+        self.assertEqual(self.harness.model.unit.status, ops.ActiveStatus())
