68f5097916607faadb924f2380a2b4a0e34a0df8
Alex Batisse <alex.batisse@canonical.com>
2025-01-06 14:47:37 +0100

    tests: Update tests to scenario v7

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,9 +3,10 @@
 # See LICENSE file for licensing details.
 
 import json
+from typing import cast
 from unittest.mock import patch
 
-from scenario import Context, State
+from ops.testing import Context, State
 from src.charm import KarapaceCharm
 from src.literals import Status
 
@@ -56,7 +57,7 @@ def patched_write_side_effects(*args, **kwargs):
 def test_install_blocks_snap_install_failure(ctx: Context):
     state_in = State()
     with patch("workload.KarapaceWorkload.install", return_value=False):
-        state_out: State = ctx.run("install", state_in)
+        state_out: State = ctx.run(ctx.on.install(), state_in)
 
     assert state_out.unit_status == Status.SNAP_NOT_INSTALLED.value.status
 
@@ -64,21 +65,21 @@ def test_install_blocks_snap_install_failure(ctx: Context):
 def test_install_snap(ctx: Context):
     state_in = State()
     with patch("workload.KarapaceWorkload.install", return_value=True):
-        state_out: State = ctx.run("install", state_in)
+        state_out: State = ctx.run(ctx.on.install(), state_in)
 
     assert state_out.unit_status != Status.SNAP_NOT_INSTALLED.value.status
 
 
 def test_start_no_peer_relation(ctx: Context):
     state_in = State()
-    state_out: State = ctx.run("start", state_in)
+    state_out: State = ctx.run(ctx.on.start(), state_in)
 
     assert state_out.unit_status == Status.NO_PEER_RELATION.value.status
 
 
 def test_start_defers_if_no_credentials_and_no_leader(ctx: Context, peer_relation_no_data):
     state_in = State(relations=[peer_relation_no_data])
-    state_out: State = ctx.run("start", state_in)
+    state_out: State = ctx.run(ctx.on.start(), state_in)
 
     assert len(state_out.deferred) == 1
     assert state_out.deferred[0].name == "start"
@@ -91,8 +92,8 @@ def test_start_creates_credentials(
     patched_workload_write.side_effect = patched_write_side_effects
     state_in = State(relations=[peer_relation_no_data], leader=True)
 
-    with ctx.manager("start", state_in) as manager:
-        charm: KarapaceCharm = manager.charm
+    with ctx(ctx.on.start(), state_in) as manager:
+        charm: KarapaceCharm = cast(KarapaceCharm, manager.charm)
         manager.run()
 
         # NOTE side_effect of patched write will already assert expected output as well
@@ -105,7 +106,7 @@ def test_start_updates_credentials_when_no_leader(
     patched_exec.side_effect = patched_exec_side_effects
     patched_workload_write.side_effect = patched_write_side_effects
     state_in = State(relations=[peer_relation])
-    ctx.run("start", state_in)
+    ctx.run(ctx.on.start(), state_in)
 
     # NOTE side_effect of patched write will already assert expected output as well
     patched_workload_write.assert_called_once()
@@ -113,21 +114,21 @@ def test_start_updates_credentials_when_no_leader(
 
 def test_ready_to_start_no_peer_relation(ctx: Context):
     state_in = State(leader=True)
-    state_out: State = ctx.run("config_changed", state_in)
+    state_out: State = ctx.run(ctx.on.config_changed(), state_in)
 
     assert state_out.unit_status == Status.NO_PEER_RELATION.value.status
 
 
 def test_ready_to_start_kafka_not_related(ctx: Context, peer_relation_no_data):
     state_in = State(relations=[peer_relation_no_data], leader=True)
-    state_out: State = ctx.run("config_changed", state_in)
+    state_out: State = ctx.run(ctx.on.config_changed(), state_in)
 
     assert state_out.unit_status == Status.KAFKA_NOT_RELATED.value.status
 
 
 def test_ready_to_start_kafka_no_data(ctx: Context, peer_relation_no_data, kafka_relation_no_data):
     state_in = State(relations=[peer_relation_no_data, kafka_relation_no_data], leader=True)
-    state_out: State = ctx.run("config_changed", state_in)
+    state_out: State = ctx.run(ctx.on.config_changed(), state_in)
 
     assert state_out.unit_status == Status.KAFKA_NO_DATA.value.status
 
@@ -136,7 +137,7 @@ def test_ready_to_start_no_internal_credentials(
     ctx: Context, peer_relation_no_data, kafka_relation
 ):
     state_in = State(relations=[peer_relation_no_data, kafka_relation], leader=True)
-    state_out: State = ctx.run("config_changed", state_in)
+    state_out: State = ctx.run(ctx.on.config_changed(), state_in)
 
     assert state_out.unit_status == Status.NO_CREDS.value.status
 
@@ -151,7 +152,7 @@ def test_config_changed_succeeds(
 ):
     patched_exec.side_effect = patched_exec_side_effects
     state_in = State(relations=[peer_relation, kafka_relation], leader=True)
-    state_out = ctx.run("config_changed", state_in)
+    state_out = ctx.run(ctx.on.config_changed(), state_in)
 
     patched_restart.assert_called_once()
     assert state_out.unit_status == Status.ACTIVE.value.status
@@ -160,7 +161,7 @@ def test_config_changed_succeeds(
 def test_update_status_blocks_if_not_healthy(ctx: Context, peer_relation, kafka_relation):
     state_in = State(relations=[peer_relation, kafka_relation], leader=True)
     with patch("workload.KarapaceWorkload.active", return_value=False):
-        state_out = ctx.run("update_status", state_in)
+        state_out = ctx.run(ctx.on.update_status(), state_in)
 
     assert state_out.unit_status == Status.SERVICE_NOT_RUNNING.value.status
 
@@ -171,7 +172,7 @@ def test_update_status_blocks_if_kafka_not_connected(ctx: Context, peer_relation
         patch("managers.kafka.KafkaManager.brokers_active", return_value=False),
         patch("workload.KarapaceWorkload.active", return_value=True),
     ):
-        state_out = ctx.run("update_status", state_in)
+        state_out = ctx.run(ctx.on.update_status(), state_in)
 
     assert state_out.unit_status == Status.KAFKA_NOT_CONNECTED.value.status
 
@@ -190,6 +191,6 @@ def test_update_status_succeeds(
         patch("managers.kafka.KafkaManager.brokers_active", return_value=True),
         patch("workload.KarapaceWorkload.active", return_value=True),
     ):
-        state_out = ctx.run("update_status", state_in)
+        state_out = ctx.run(ctx.on.update_status(), state_in)
 
     assert state_out.unit_status == Status.ACTIVE.value.status
