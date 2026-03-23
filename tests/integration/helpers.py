2786be54c924f0278e9f79c01c3b6add21049741
Iman Enami <iman.enami@canonical.com>
2026-03-23 14:43:16 +0400

    refactor: use jubilant

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from subprocess import PIPE, check_output
 
 import yaml
-from pytest_operator.plugin import OpsTest
+from jubilant_adapters import JujuFixture
 
 from literals import PORT
 
@@ -27,51 +27,51 @@ SERIES = "noble"
 CA_FILE = "/tmp/ca-cert.pem"
 
 
-async def get_admin_credentials(ops_test: OpsTest, num_unit=0) -> str:
+def get_admin_credentials(juju: JujuFixture, num_unit=0) -> str:
     """Use the charm action to retrieve the password for admin user.
 
     Return:
         String with the password stored on the peer relation databag.
     """
-    action = await ops_test.model.units.get(f"{APP_NAME}/{num_unit}").run_action("get-password")
-    password = await action.wait()
+    action = juju.ext.model.units.get(f"{APP_NAME}/{num_unit}").run_action("get-password")
+    password = action.wait()
     return password.results["password"]
 
 
-async def set_password(ops_test: OpsTest, username="operator", password=None, num_unit=0) -> str:
+def set_password(juju: JujuFixture, username="operator", password=None, num_unit=0) -> dict:
     """Use the charm action to start a password rotation."""
     params = {"username": username}
     if password:
         params["password"] = password
 
-    action = await ops_test.model.units.get(f"{APP_NAME}/{num_unit}").run_action(
+    action = juju.ext.model.units.get(f"{APP_NAME}/{num_unit}").run_action(
         "set-password", **params
     )
-    password = await action.wait()
+    password = action.wait()
     return password.results
 
 
-async def get_application_credentials(ops_test: OpsTest, role="user") -> tuple[str, str]:
-    action = await ops_test.model.units.get(f"{DUMMY_NAME}/0").run_action(
+def get_application_credentials(juju: JujuFixture, role="user") -> tuple[str, str]:
+    action = juju.ext.model.units.get(f"{DUMMY_NAME}/0").run_action(
         "get-credentials", **{"username": role}
     )
-    credentials = await action.wait()
+    credentials = action.wait()
     return credentials.results["username"], credentials.results["password"]
 
 
-async def set_tls_private_key(ops_test: OpsTest, key: str | None = None, num_unit=0):
+def set_tls_private_key(juju: JujuFixture, key: str | None = None, num_unit=0):
     """Use the charm action to start a password rotation."""
     params = {"internal-key": key} if key else {}
 
-    action = await ops_test.model.units.get(f"{APP_NAME}/{num_unit}").run_action(
+    action = juju.ext.model.units.get(f"{APP_NAME}/{num_unit}").run_action(
         "set-tls-private-key", **params
     )
-    return (await action.wait()).results
+    return (action.wait()).results
 
 
-async def get_address(ops_test: OpsTest, app_name=APP_NAME, unit_num=0) -> str:
+def get_address(juju: JujuFixture, app_name=APP_NAME, unit_num=0) -> str:
     """Get the address for a unit."""
-    status = await ops_test.model.get_status()  # noqa: F821
+    status = juju.ext.model.get_status()  # noqa: F821
     address = status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["public-address"]
     return address
 
@@ -81,13 +81,11 @@ def check_socket(host: str, port: int) -> bool:
         return sock.connect_ex((host, port)) == 0
 
 
-async def assert_list_schemas(
-    ops_test: OpsTest, expected_schemas: str = "[]", units: int = 1
-) -> None:
+def assert_list_schemas(juju: JujuFixture, expected_schemas: str = "[]", units: int = 1) -> None:
     """Assert schemas can be listed."""
-    operator_password = await get_admin_credentials(ops_test)
+    operator_password = get_admin_credentials(juju)
     for i in range(units):
-        address = await get_address(ops_test=ops_test, unit_num=i)
+        address = get_address(juju=juju, unit_num=i)
         command = " ".join(
             [
                 "curl",
--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -2,10 +2,8 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import asyncio
 import logging
 
-import pytest
 import requests
 from helpers import (
     APP_NAME,
@@ -17,7 +15,7 @@ from helpers import (
     get_admin_credentials,
     set_tls_private_key,
 )
-from pytest_operator.plugin import OpsTest
+from jubilant_adapters import JujuFixture, gather
 
 from literals import PORT
 
@@ -27,80 +25,74 @@ TLS_NAME = "self-signed-certificates"
 TLS_CHANNEL = "1/stable"
 
 
-@pytest.mark.abort_on_fail
-@pytest.mark.skip_if_deployed
-async def test_deploy_tls(ops_test: OpsTest, karapace_charm):
+def test_deploy_tls(juju: JujuFixture, karapace_charm):
     tls_config = {"ca-common-name": "kafka"}
 
-    await asyncio.gather(
-        ops_test.model.deploy(karapace_charm, application_name=APP_NAME, series=SERIES),
-        ops_test.model.deploy(TLS_NAME, channel=TLS_CHANNEL, config=tls_config),
-        ops_test.model.deploy(ZOOKEEPER, channel="3/stable", application_name=ZOOKEEPER),
-        ops_test.model.deploy(KAFKA, channel="3/stable", application_name=KAFKA),
+    gather(
+        juju.ext.model.deploy(karapace_charm, application_name=APP_NAME, series=SERIES),
+        juju.ext.model.deploy(TLS_NAME, channel=TLS_CHANNEL, config=tls_config),
+        juju.ext.model.deploy(ZOOKEEPER, channel="3/stable", application_name=ZOOKEEPER),
+        juju.ext.model.deploy(KAFKA, channel="3/stable", application_name=KAFKA),
     )
-    await ops_test.model.wait_for_idle(
+    juju.ext.model.wait_for_idle(
         apps=[APP_NAME, ZOOKEEPER, KAFKA, TLS_NAME], idle_period=20, timeout=1800
     )
 
-    assert ops_test.model.applications[APP_NAME].status == "blocked"
-    assert ops_test.model.applications[KAFKA].status == "blocked"
-    assert ops_test.model.applications[ZOOKEEPER].status == "active"
-    assert ops_test.model.applications[TLS_NAME].status == "active"
+    assert juju.ext.model.applications[APP_NAME].status == "blocked"
+    assert juju.ext.model.applications[KAFKA].status == "blocked"
+    assert juju.ext.model.applications[ZOOKEEPER].status == "active"
+    assert juju.ext.model.applications[TLS_NAME].status == "active"
 
     # Relate Zookeeper & Kafka to TLS
-    await ops_test.model.add_relation(KAFKA, ZOOKEEPER)
-    await ops_test.model.add_relation(TLS_NAME, ZOOKEEPER)
-    await ops_test.model.add_relation(TLS_NAME, f"{KAFKA}:certificates")
+    juju.ext.model.add_relation(KAFKA, ZOOKEEPER)
+    juju.ext.model.add_relation(TLS_NAME, ZOOKEEPER)
+    juju.ext.model.add_relation(TLS_NAME, f"{KAFKA}:certificates")
 
-    async with ops_test.fast_forward(fast_interval="60s"):
-        await ops_test.model.wait_for_idle(
+    with juju.ext.fast_forward(fast_interval="60s"):
+        juju.ext.model.wait_for_idle(
             apps=[TLS_NAME, ZOOKEEPER, KAFKA], idle_period=25, timeout=1800, status="active"
         )
 
-    assert ops_test.model.applications[TLS_NAME].status == "active"
-    assert ops_test.model.applications[ZOOKEEPER].status == "active"
-    assert ops_test.model.applications[KAFKA].status == "active"
+    assert juju.ext.model.applications[TLS_NAME].status == "active"
+    assert juju.ext.model.applications[ZOOKEEPER].status == "active"
+    assert juju.ext.model.applications[KAFKA].status == "active"
 
 
-@pytest.mark.abort_on_fail
-async def test_karapace_tls(ops_test: OpsTest):
+def test_karapace_tls(juju: JujuFixture):
     """Tests TLS on Karapace."""
     # Relate Kafka[TLS] to Karapace[Non-TLS]
-    await ops_test.model.add_relation(KAFKA, APP_NAME)
-    await ops_test.model.wait_for_idle(apps=[KAFKA], idle_period=15, timeout=1000, status="active")
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME], idle_period=15, timeout=1000, status="blocked"
-    )
+    juju.ext.model.add_relation(KAFKA, APP_NAME)
+    juju.ext.model.wait_for_idle(apps=[KAFKA], idle_period=15, timeout=1000, status="active")
+    juju.ext.model.wait_for_idle(apps=[APP_NAME], idle_period=15, timeout=1000, status="blocked")
 
     # Unit is on 'blocked' but whole app is on 'waiting'
-    assert ops_test.model.applications[APP_NAME].status == "blocked"
+    assert juju.ext.model.applications[APP_NAME].status == "blocked"
 
     # Set a custom private key, by running set-tls-private-key action with no parameters,
     # as this will generate a random one
-    await set_tls_private_key(ops_test)
+    set_tls_private_key(juju)
 
     logger.info("Relate Karapace to TLS")
-    await ops_test.model.add_relation(APP_NAME, TLS_NAME)
-    await ops_test.model.wait_for_idle(
+    juju.ext.model.add_relation(APP_NAME, TLS_NAME)
+    juju.ext.model.wait_for_idle(
         apps=[APP_NAME, KAFKA, TLS_NAME], idle_period=30, timeout=1200, status="active"
     )
 
-    assert ops_test.model.applications[APP_NAME].status == "active"
-    assert ops_test.model.applications[KAFKA].status == "active"
+    assert juju.ext.model.applications[APP_NAME].status == "active"
+    assert juju.ext.model.applications[KAFKA].status == "active"
 
 
-@pytest.mark.abort_on_fail
-async def test_schema_creation(ops_test: OpsTest):
+def test_schema_creation(juju: JujuFixture):
     """Check that a schema can be registered using internal credentials."""
     # Store the CA cert for requests
-    action = await ops_test.model.units.get(f"{TLS_NAME}/0").run_action("get-ca-certificate")
-    ca = await action.wait()
+    action = juju.ext.model.units.get(f"{TLS_NAME}/0").run_action("get-ca-certificate")
+    ca = action.wait()
     ca = ca.results.get("ca-certificate")
     open(CA_FILE, "w").write(ca)
 
     schema_name = "test-key"
-    operator_password = await get_admin_credentials(ops_test)
-    address = await get_address(ops_test=ops_test)
+    operator_password = get_admin_credentials(juju)
+    address = get_address(juju=juju)
     base_url = f"https://{address}:{PORT}"
     auth = ("operator", operator_password)
 
